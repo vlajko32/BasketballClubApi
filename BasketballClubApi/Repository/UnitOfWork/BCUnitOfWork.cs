7114d66c4359826ba184b4e39b5d391aa7dd14fc
vlajko32 <vlajko247@gmail.com>
2022-02-11 14:03:31 +0100

    Dodat UnitOfWork folder koji sadrzi interfejs i implementaciju klase BCUnitOfWork koja sluzi za realizaciju istoimenog patterna

---
+++ BasketballClubApi/Repository/UnitOfWork/BCUnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using BasketballClub_Rest.Domain;
+using BasketballClub_Rest.Repository.iRepository;
+using BasketballClub_Rest.Repository.Repository_Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketballClub_Rest.Repository.UnitOfWork
+{
+    public class BCUnitOfWork : IUnitOfWork
+    {
+        private BCContext context;
+
+        public BCUnitOfWork(BCContext context)
+        {
+            this.context = context;
+
+            Coaches = new RepositoryCoach(context);
+            Administrators = new RepositoryAdministrator(context);
+            Users = new RepositoryUser(context);
+            Players = new RepositoryPlayer(context);
+            Selections = new RepositorySelection(context);
+            Trainings = new RepositoryTraining(context);
+            Gyms = new RepositoryGym(context);
+        }
+
+        public IRepositoryCoach Coaches { get; set;  }
+        public IRepositoryAdministrator Administrators { get; set; }
+        public IRepositoryPlayer Players { get; set; }
+        public IRepositorySelection Selections { get; set; }
+        public IRepositoryTraining Trainings { get; set; }
+
+        public IRepositoryGym Gyms { get; set; }
+        public IRepositoryUser Users { get; set; }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+         }
+    }
+}
---
+++ BasketballClubApi/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using BasketballClub_Rest.Repository.iRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketballClub_Rest.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+       public IRepositoryCoach Coaches { get; set; }
+
+        public IRepositoryAdministrator Administrators { get; set; }
+
+        public IRepositoryPlayer Players { get; set; }
+
+        public IRepositorySelection Selections { get; set; }
+
+        public IRepositoryTraining Trainings { get; set; }
+
+        public IRepositoryUser Users { get; set; }
+
+        public IRepositoryGym Gyms { get; set; }
+
+        void Commit();
+
+        void Dispose();
+    }
+}
