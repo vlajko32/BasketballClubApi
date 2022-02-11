d1b923e331dfea8c62e09b5b79dd0311355160e0
vlajko32 <vlajko247@gmail.com>
2022-02-11 14:14:55 +0100

    Projekat spremljen za rad sa JWT tokenima i implementaciju autorizacije

--- BasketballClubApi/Startup.cs
+++ BasketballClubApi/Startup.cs
@@ -1,5 +1,7 @@
 using BasketballClub_Rest.Domain;
 using BasketballClub_Rest.Repository.UnitOfWork;
+using BasketballClub_Rest.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,10 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BasketballClubApi
@@ -30,16 +35,43 @@ namespace BasketballClubApi
         {
             services.AddCors();
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(
+               opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BasketballClubApi", Version = "v1" });
             });
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+              .AddJwtBearer(options =>
+              {
+
+
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = false,
+                      ValidateAudience = false,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+
+                      IssuerSigningKey = new SymmetricSecurityKey(
+                          Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JWTSecretKey"))
+                      )
+                  };
+
+              });
             services.AddDbContext<BCContext>();
             services.AddScoped<IUnitOfWork, BCUnitOfWork>();
 
+            services.AddSingleton<IAuthService>(new AuthService(
+                    new BCUnitOfWork(new BCContext()),
+                    Configuration.GetValue<string>("JWTSecretKey"),
+                    Configuration.GetValue<int>("JWTLifespan")
 
+                ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
