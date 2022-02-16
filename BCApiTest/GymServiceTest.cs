ca10ab5a02881c9124e5076599bf4b8c3afa1a8d
vlajko32 <vlajko247@gmail.com>
2022-02-16 16:56:47 +0100

    Kreiran test za klasu GymService

---
+++ BCApiTest/GymServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using BasketballClub_Rest.Domain;
+using BasketballClub_Rest.Repository.UnitOfWork;
+using BasketballClubApi.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BCApiTest
+{
+    public class GymServiceTest : IDisposable
+    {
+        private IUnitOfWork uow;
+        private BCContext context;
+        private GymService gymService;
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        public GymServiceTest()
+        {
+            DbContextOptionsBuilder dbContextOption = new DbContextOptionsBuilder<BCContext>().UseInMemoryDatabase(new Guid().ToString());
+            context = new BCContext(dbContextOption.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options);
+            context.Database.EnsureCreated();
+            uow = new BCUnitOfWork(context);
+            gymService = new GymService(uow);
+        }
+
+
+
+        [Fact]
+        public void GetAllShouldReturnAllGyms()
+        {
+
+            List<Gym> gyms = gymService.GetAll();
+
+            Assert.Equal(3, gyms.Count);
+            Dispose();
+        }
+    }
+}
